34c494f888a0af1234cd32d9f8b7163b6df1716a
mseon94 <kkamnyang94@gmail.com>
2026-09-14 17:00:26 +0900

    frontend 개발 환경 설정

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// 우리가 실제 화면을 만드는 중심 파일
+import './App.css'
+
+// 화면을 만드는 함수
+function App() {
+  // 실제 브라우저에 보여줄 내용
+  return (
+    <div>
+      <h1>Meal Fit</h1>
+      <p>식재료 가격 예측 시스템</p>
+      <p>테스트용</p>
+    </div>
+  )
+}
+
+export default App
